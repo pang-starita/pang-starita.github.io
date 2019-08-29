ab89ced70c12788c563531d44fbc649023fac57e
Paulino Ng <pang.starita@gmail.com>
2019-08-28 22:21:53 -0300

    Correçãozinha

--- convHtml.bat
+++ convHtml.bat
@@ -1,3 +1,3 @@
 pandoc -f markdown -s -t html -o LP0.html LP0.md
 pandoc -f markdown -s -t html -o LP1.html LP1.md
-pandoc -f markdown -s -t html -o LP1.htm2 LP2.md
+pandoc -f markdown -s -t html -o LP2.html LP2.md
