03c19394df3b9d412d62078b3428e642eb5e8c23
Paulino Ng <pang.starita@gmail.com>
2019-09-26 22:02:53 -0300

    Recompilaçao das LP*.md e fibo.zip (Fibonacci em C).

--- convHtml.bat
+++ convHtml.bat
@@ -1,3 +1,8 @@
-pandoc -f markdown -s -t html -o LP0.html LP0.md
-pandoc -f markdown -s -t html -o LP1.html LP1.md
-pandoc -f markdown -s -t html -o LP2.html LP2.md
+@echo off
+
+rem isto e um comentario
+
+where pandoc
+IF ERRORLEVEL 1 CALL %USERPROFILE%\noserver.bat
+
+FOR %%f IN (L*.md) DO pandoc -f markdown -s -t html -o %%~nf.html %%~nf.md
